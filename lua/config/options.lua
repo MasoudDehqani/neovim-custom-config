3dec4a15e162344affac8cb21f6f25cf45f9960e
Masoud <masoud67.md@gmail.com>
2024-09-25 18:33:15 +0330

    bufferline plugin added

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,10 +12,12 @@ vim.wo.relativenumber = true
 vim.opt.termbidi = true
 vim.opt.encoding = "utf-8"
 -- vim.opt.fileencoding = "utf-8"
-vim.opt.guifont = "Vazirmatn"
+-- vim.opt.guifont = "Vazirmatn"
 
 -- Tab
 vim.opt.tabstop = 2 -- number of visual spaces per TAB
 vim.opt.softtabstop = 2 -- number of spacesin tab when editing
 vim.opt.shiftwidth = 2 -- insert 4 spaces on a tab
 vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+
+vim.opt.termguicolors = true
