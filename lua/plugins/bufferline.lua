7cc4bb8e54faf436acfe36f80f6d34f39af456cd
Masoud <masoud67.md@gmail.com>
2024-09-26 15:09:50 +0330

    config of bufferline plugin edited

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,11 +2,17 @@ return {
 	"akinsho/bufferline.nvim",
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
+	opts = {
+		options = {
+			mode = "tabs",
+			separator_style = "slant",
+		},
+	},
 	config = function()
 		require("bufferline").setup({
 			options = {
 				diagnostics = "nvim_lsp",
-				diagnostics_indicator = function(count, level, diagnostics_dict, context)
+				diagnostics_indicator = function(count, level)
 					local icon = level:match("error") and " " or " "
 					return " " .. icon .. count
 				end,
