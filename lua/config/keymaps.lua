ddc55ea7eba1c6a5079b640a52d790855af17306
Masoud <masoud67.md@gmail.com>
2024-09-26 12:47:46 +0330

    keymaps of telescope refactored, todo-comments plugin added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,17 +3,6 @@ local keymap = vim.keymap -- for conciseness
 -- set leader key to space
 vim.g.mapleader = " "
 
--- Telescope keymaps
-local builtin = require("telescope.builtin")
-keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-
-keymap.set("n", "gtd", "<cmd>Telescope lsp_definitions<cr>")
-keymap.set("n", "gtr", "<cmd>Telescope lsp_references<cr>")
-keymap.set("n", "gti", "<cmd>Telescope lsp_implementations<cr>")
-
 ---------------------
 -- General Keymaps -------------------
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 		"nvim-tree/nvim-web-devicons",
+		"folke/todo-comments.nvim",
 	},
 	config = function()
 		local telescope = require("telescope")
@@ -12,7 +13,7 @@ return {
 
 		telescope.setup({
 			defaults = {
-				path_display = { "smart" },
+				-- path_display = { "smart" },
 				mappings = {
 					i = {
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
@@ -32,5 +33,14 @@ return {
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+
+		keymap.set("n", "gtd", "<cmd>Telescope lsp_definitions<cr>")
+		keymap.set("n", "gtr", "<cmd>Telescope lsp_references<cr>")
+		keymap.set("n", "gti", "<cmd>Telescope lsp_implementations<cr>")
+
+		local builtin = require("telescope.builtin")
+		keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 	end,
 }
