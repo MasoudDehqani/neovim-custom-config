f623b14be7c8b46cd620c1f0683ec47ba7498fa7
Masoud <masoud67.md@gmail.com>
2024-09-28 22:38:20 +0330

    alfa plugin replaced by dashboard, config of cmp edited, colorscheme config edited

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -10,6 +10,10 @@ return {
 		local cmp = require("cmp")
 
 		cmp.setup({
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
 			completion = {
 				completeopt = "menu, menuone, preview, noselect",
 			},
