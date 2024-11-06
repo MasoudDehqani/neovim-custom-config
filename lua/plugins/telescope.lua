dd5f64a027e5ca4a745c93055cd0d58918ba21cd
Masoud <masoud67.md@gmail.com>
2024-11-06 09:06:58 +0330

    keymap added to telescope for git status search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -47,9 +47,10 @@ return {
 
 		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
 		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+		keymap.set("n", "<leader>fg", "<cmd>Telescope git_status<cr>", { desc = "Find git status" })
 
 		keymap.set("n", "gtd", "<cmd>Telescope lsp_definitions<cr>")
 		keymap.set("n", "gtr", "<cmd>Telescope lsp_references<cr>")
